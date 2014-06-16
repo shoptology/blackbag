76e46f59f36c9f7606051274f70c578b587f7aa4
ivanmayes <ivanmayes@gmail.com>
2014-06-16 16:35:25 -0500

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var config =  {
+	db : {
+		type : 'mysql',
+		address : 'apps.m11dev.com',
+		database : 'bolddental',
+		table_prefix : '',
+		username : 'root',
+		password : '2#perday'
+	},
+	exports : {
+		exporter : 'wordpress',
+		map : {
+			meta : [
+				'ID',
+				'post_author',
+				'post_date',
+				'post_title',
+				'post_excerpt',
+				'post_name',
+				'post_type'
+			],
+			body : [
+				'post_content'
+			]
+		},
+		data : [
+			{
+				from : 'wp_posts, wp_postmeta, wp_comments',
+				where : {
+					"wp_posts.ID" : 'wp_postmet.post_id',
+					'wp_posts.post_type' : 'page',
+					post_author : '1'
+				},
+				to : 'documents/posts',
+				format : 'json',
+				filename: '{post_title}',
+				map : {
+					
+				}
+			},
+			{
+				from : 'wp_posts',
+				where : {
+					post_type : ['page']
+				},
+				to : 'delete',
+				format : 'json',
+				filename: '{post_title}'
+			}
+		]
+	},
+	filenameSpaceReplace : '-'
+};
+
+module.exports = config;
