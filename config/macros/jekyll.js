6bca0c2434ff0c85e7daf3bb3ae6acb4e60073d7
ivanmayes <ivanmayes@gmail.com>
2014-06-17 11:15:28 -0500

    Added macro configs

---
+++ config/macros/jekyll.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var jekyll_config =  {
+	prependDate: 'post_date',
+	prependDateFormat: 'YYYY-MM-DD',
+	filename: '{post_title}.html',
+	filenameSeparator : '-'
+};
+
+module.exports = jekyll_config;
