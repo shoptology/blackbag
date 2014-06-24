a87dabf7d2a307703134702ecceaa92cfa39401c
Michael May <michael.may@goshoptology.com>
2014-06-24 11:07:56 -0500

    Added drupal exporter; Changed query_hook to pass DB object;

---
+++ config/exporters/drupal.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var exporter = {
+	config : {
+		options : {
+			query_continue : false
+		}
+	},
+	export : function(query_object, database) {
+
+
+		return '**SOME-DATA***';
+	},
+	get : {
+		posts : {}
+	}
+};
+
+module.exports = exporter;
